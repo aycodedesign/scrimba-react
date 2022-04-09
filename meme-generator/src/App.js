0926f79d7e7bf3a518ba55288bd277664da87786
el3ctricSol <aubrey.ky.young@gmail.com>
2022-04-08 18:32:02 -0700

    5:12 on the course

--- meme-generator/src/App.js
+++ meme-generator/src/App.js
@@ -2,11 +2,24 @@ import React from "react";
 import Header from "./components/Header";
 import Meme from "./components/Meme";
 
+// export default function App() {
+// 	return (
+// 		<div>
+// 			<Header />
+// 			<Meme />
+// 		</div>
+// 	);
+// }
+
 export default function App() {
+	const thingsArray = ["Thing 1", "Thing 2"];
+
+	const thingsElements = thingsArray.map((thing) => <p key={thing}>{thing}</p>);
+
 	return (
 		<div>
-			<Header />
-			<Meme />
+			<button>Add Item</button>
+			{thingsElements}
 		</div>
 	);
 }
--- meme-generator/src/components/Meme.js
+++ meme-generator/src/components/Meme.js
@@ -1,11 +1,25 @@
 import React from "react";
 import MemeImg from "../images/memeimg.png";
 import NewMemeImg from "../images/newmemeimg.png";
+import memesData from "../memesData";
 
 export default function Meme() {
+	function getMemeImage() {
+		const memesArray = memesData.data.memes;
+		const randomNumber = Math.floor(Math.random() * memesArray.length);
+
+		const url = memesArray[randomNumber].url;
+
+		console.log(url);
+	}
+
+	function handleMouseOver() {
+		console.log("OVERED");
+	}
+
 	return (
 		<div className='container'>
-			<form className='form'>
+			<div className='form'>
 				<input
 					type='text'
 					id='toptext'
@@ -20,12 +34,17 @@ export default function Meme() {
 					placeholder='Bottom text'
 				/>
 				{/* <p>and take my money</p> */}
-			</form>
-			<button>
+			</div>
+			<button onClick={getMemeImage}>
 				<img src={NewMemeImg} alt='Get a new meme' />
 			</button>
 			<div className='container--meme'>
-				<img src={MemeImg} alt='meme' />
+				<img
+					// onMouseOver={handleMouseOver}
+					className='container--img'
+					src={MemeImg}
+					alt='meme'
+				/>
 				<span className='container--meme_top'>
 					<b>SHUT UP</b>
 				</span>
